0cc3364815ca56d2576b65b37faaa1bb63115763
DRudnev <dmitrurudnev@gmail.com>
2025-07-07 18:59:15 +0300

    rand

--- lesson3.py
+++ lesson3.py
@@ -1,4 +1,17 @@
-print("hello City")
-print("hello City")
-print("hello City")
-print("hello City")
+
+from random import choice
+list1 = (1, 2, 3)
+list2 = ("кошка", "собака", "конь")
+list3 = ("ребенок", "женшина", "Мужчина")
+
+
+def rand():
+    a = choice(list1)
+    b = choice(list2)
+    c = choice(list3)
+    return a, b, c
+
+resul = rand()
+print(resul)
+
+
